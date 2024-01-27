cfef71e171fafda796093dc9a827faaf2ce56a30
yusakukai <kaiso@kaisokaiso.com>
2024-01-27 14:52:00 +0900

    webpack + babel の環境でエントリーポイントをつくりました。

---
+++ sample-02/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Develop. Preview. Ship. 🚀</h1>
+			</div>
+		);
+	}
+}
+ReactDOM.render(<App />, document.querySelector('#app'));
